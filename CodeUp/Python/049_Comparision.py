05b275dc3d97981f6dac1dfab1c5c5a6cfa9fa2f
DoranLyong <cheeryun@gmail.com>
2020-10-05 10:45:29 +0900

    comparision

---
+++ CodeUp/Python/049_Comparision.py
@@ -0,0 +1,16 @@
+"""
+[ref] https://codeup.kr/problem.php?id=1049
+
+Question:
+    1. take two integers; (a, b)
+    2. a > b -> print 1 
+    3. a <= b -> print 0 
+"""
+import sys 
+
+a, b = map(int, sys.stdin.readline().rstrip().split())
+
+
+output = 1 if a > b else 0 
+
+print(output)
