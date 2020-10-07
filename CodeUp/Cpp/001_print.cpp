1f81ca824eadb604a043427ec7f1501bc7100c78
DoranLyong <cheeryun@gmail.com>
2020-10-07 15:56:14 +0900

    print.cpp

---
+++ CodeUp/Cpp/001_print.cpp
@@ -0,0 +1,14 @@
+/*
+[ref] https://codeup.kr/problem.php?id=1001
+
+Question:
+    Print 'Hello' 
+*/
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello \n");
+
+    return 0; 
+}
---
+++ CodeUp/Cpp/002_print.cpp
@@ -0,0 +1,14 @@
+/*
+[ref] https://codeup.kr/problem.php?id=1002
+
+Question:
+    Print "Hello World" 
+*/ 
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World \n");
+
+    return 0;
+}
