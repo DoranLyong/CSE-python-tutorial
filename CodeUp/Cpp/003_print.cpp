74dd80d6d452e715d4aabc69bf65d855ea8f3fbd
DoranLyong <cheeryun@gmail.com>
2020-10-07 16:39:46 +0900

    print.cpp

---
+++ CodeUp/Cpp/003_print.cpp
@@ -0,0 +1,16 @@
+/*
+[ref] https://codeup.kr/problem.php?id=1003
+
+Question:
+    1. Line change 
+    2. Print as "Hello"
+                "World" 
+*/ 
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello \nWorld");
+
+    return 0;
+}
